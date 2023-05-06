5ac33cfa46022f2d5ec16f5194cd62ab0e08c459
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-06 10:53:09 +0700

    hien thi 20 so nguyen to dau tien

--- TwentyFirstPrime.java
+++ TwentyFirstPrime.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class TwentyFirstPrime {
+import java.util.Scanner;
+
+public class TwentyFirstPrime {
+    public static void main(String[] args) {
+        int count = 0;
+        System.out.println("20 so nguyen to dau tien : ");
+        for (int i = 2; count < 20; i++) {
+            boolean isPrime = true;
+            for(int j=2;j<i/2;j++){
+                if(i%j==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime==true){
+                System.out.println(i+ "\t");
+                count++;
+            }
+        }
+    }
 }
